860381bbd98f893b5558fcde53175115d437a3fa
Arjun <arjun.bali96@gmail.com>
2014-06-06 10:55:00 -0400

    call_option

--- crm.rb
+++ crm.rb
@@ -13,3 +13,12 @@ def main_menu
   selection = get.chomp.to_i
   call_option(selection)
 end
+
+def call_option(selection)
+	add_new_contact if selection == 1
+	modify_existing_contact if selection == 2
+	delete_existing_contact if selection == 3
+	display_contacts if selection == 4
+	display_attribute if selection == 5
+	exit_crm if selection == 6
+end 
